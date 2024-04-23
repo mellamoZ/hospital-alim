4bfeeca6e9c1d7d511a2d6dbdce67af037a90b22
mellamoz <abdulazizmohamed959@gmail.com>
2024-04-23 17:04:15 +0300

    done  it 2

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 import React from "react";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/config/authOptions";
 import { redirect } from "next/navigation";
 
 const DashboardPage = async () => {
--- app/dashboard/wards/page.tsx
+++ app/dashboard/wards/page.tsx
@@ -1,7 +1,7 @@
 import { getWards } from "@/lib/db/wardCrud";
 import WardTable from "./WardTable";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/config/authOptions";
 import { redirect } from "next/navigation";
 
 export default async function WardsPage() {
